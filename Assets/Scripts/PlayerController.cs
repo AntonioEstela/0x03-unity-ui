e3b7a90bedb0b664daddd6e9dfa6616cb6d3ea42
Antonio Estela <antonioestela73@gmail.com>
2021-01-04 15:00:44 -0500

    Added some needed files

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 50;
+    public Rigidbody rb;
+    private int score = 0;
+    public int health = 5;
+
+    void Update()
+    {
+        if (health == 0)
+        {
+            Debug.Log("Game Over!");
+            SceneManager.LoadScene("maze");
+        }
+    }
+    void FixedUpdate()
+    {
+        float XInput = Input.GetAxis("Horizontal");
+        float ZInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3 (XInput, 0.0f, ZInput);
+        
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Pickup")
+        {
+            score += 1;
+            Debug.Log($"Score: {score}");
+            other.gameObject.SetActive(false);
+        }
+
+        if (other.tag == "Trap")
+        {
+            health -= 1;
+            Debug.Log($"Health: {health}");
+        }
+
+        if (other.tag == "Goal")
+        {
+            Debug.Log("You win!");
+        }
+    }
+}
